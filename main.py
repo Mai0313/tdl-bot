80c1c25203ae168c818a23e2ce059549b0c89f8c
wei <weichenglee0313@gmail.com>
2024-10-16 21:35:16 +0800

    Refactor handle_message function to exclude post_chatname variable and update logfire message details

--- main.py
+++ main.py
@@ -20,10 +20,6 @@ async def handle_message(update: Update, context: CallbackContext) -> None:
         post_sender: str = update.message.api_kwargs["forward_from_chat"]["username"]
         # >>> Output: wanjianxiuxishi
 
-        # 聊天室名稱當資料夾名稱
-        post_chatname: str = update.message.api_kwargs["forward_from_chat"]["title"]
-        # >>> Output: 晚间休息室🔞
-
         file_url = f"https://t.me/{post_sender}/{post_id}"
 
         if update.message.photo:
@@ -36,32 +32,23 @@ async def handle_message(update: Update, context: CallbackContext) -> None:
         if update.message.text.startswith("https://t.me/"):
             post_id = update.message.text.split("/")[-1]
             post_sender = update.message.text.split("/")[-2]
-            post_chatname = "private chat"
             file_url = update.message.text
         else:
             logfire.info("Received a text message", text=update.message.text)
             return await update.message.reply_text("好色喔 但你不會用對吧 嘿嘿")
-    logfire.info(
-        "Message Details",
-        post_id=post_id,
-        post_sender=post_sender,
-        post_chatname=post_chatname,
-        url=file_url,
-    )
+    logfire.info("Message Details", post_id=post_id, post_sender=post_sender, url=file_url)
     td = DownloadProcessor(
         func="download", serve=False, skip_same=True, limit=4, pool=0, threads=8
     )
     td.run(url=file_url)
     return await update.message.reply_text(
-        f"Post ID: {post_id}\nPost Sender: {post_sender}\nChat Name: {post_chatname}\nFile URL: {file_url}"
+        f"Post ID: {post_id}\nPost Sender: {post_sender}\nFile URL: {file_url}"
     )
 
 
 # 啟動命令的處理函數
 async def start(update: Update, context: CallbackContext) -> None:
-    await update.message.reply_text(
-        "轉發訊息或提供一個連結，我會幫你下載的！"
-    )
+    await update.message.reply_text("轉發訊息或提供一個連結，我會幫你下載的!")
 
 
 # 主函數
