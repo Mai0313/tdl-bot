22e4b7e8e4005bb1b824cfdad4e27c45fd51e391
wei <weichenglee0313@gmail.com>
2024-12-15 19:04:48 +0800

    feat: refactor file handling for improved download management - Import `Path` from the `pathlib` module for file handling - Update the return statement to provide the name of the most recently downloaded file - Create an `output_path` variable to specify the downloads directory - Modify the way the downloaded filename is determined by sorting files based on their creation time
    Signed-off-by: wei <weichenglee0313@gmail.com>

--- web.py
+++ web.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import gradio as gr
 from src.tdl.processor import TDLManager
 
@@ -7,7 +9,11 @@ def greet(file_url: str, limit: int, pool: int, threads: int) -> str:
         func="download", serve=False, skip_same=True, limit=limit, pool=pool, threads=threads
     )
     td.run(url=file_url)
-    return "downloads/1128501931_36835_调教漂亮反差小女友，捆绑绑手戴上口球，跳蛋玩弄无助的扭动，口交深喉，主动上位超用力淫叫_今天6.mp4"
+    output_path = Path("downloads")
+    downloaded = (
+        "downloads/" + sorted(output_path.iterdir(), key=lambda x: x.stat().st_ctime)[-1].name
+    )
+    return downloaded
 
 
 demo = gr.Interface(fn=greet, inputs=["text", "slider", "slider", "slider"], outputs=[gr.Video()])
